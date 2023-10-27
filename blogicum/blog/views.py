83ad0d440366ca4dbc61a9675eac6b901bd2c8e0
Dmitry Yakimov <Diman.yackim@ya.ru>
2023-10-27 15:32:39 +0400

    Final 2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -12,7 +12,9 @@ def index(request):
             'location', 'location__is_published',
             'category__title', 'title'
         ).filter(
-                Q(is_published=True) & Q(category__is_published=True) & Q(pub_date__lte=dt.now())
+            Q(is_published=True)
+            & Q(category__is_published=True)
+            & Q(pub_date__lte=dt.now())
         ).order_by('pub_date')[0:5],
         is_published=False
     )
@@ -27,7 +29,9 @@ def post_detail(request):
             'location', 'location__is_published',
             'category__title', 'title', 'location__name'
         ).filter(
-            Q(is_published=True) & Q(category__is_published=True) & Q(pub_date__lte=dt.now())
+            Q(is_published=True)
+            & Q(category__is_published=True)
+            & Q(pub_date__lte=dt.now())
         ).order_by('pub_date')[0:5],
         (Q(is_published=False)
          | Q(pub_date__gte=dt.now())
@@ -42,9 +46,13 @@ def category_posts(request):
     template = 'blog/category.html'
     category = get_object_or_404(
         Post.objects.values(
-            'category__description', 'category__title', 'category__slug__is_published',
+            'category__description',
+            'category__title',
+            'category__slug__is_published',
         ).filter(
-            Q(is_published=True) & Q(category__is_published=True) & Q(pub_date__lte=dt.now())
+            Q(is_published=True)
+            & Q(category__is_published=True)
+            & Q(pub_date__lte=dt.now())
         ).order_by('pub_date')[0:5],
         category__is_published=False
     )
