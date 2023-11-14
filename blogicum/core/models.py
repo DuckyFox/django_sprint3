407d0dfd5601ed34b92858510fb76c850b29498f
Dmitry Yakimov <Diman.yackim@ya.ru>
2023-11-15 00:35:46 +0400

    Reade Steady GO v2

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -5,7 +5,6 @@ class BaseModel(models.Model):
     is_published = models.BooleanField(
       default=True,
       verbose_name='Опубликовано',
-
       help_text='Снимите галочку, чтобы скрыть публикацию.')
     created_at = models.DateTimeField(
       auto_now_add=True,
